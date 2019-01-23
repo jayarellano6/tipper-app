8bf72bfdbe837dc7bd84756374707fe13af89d11
Jay Arellano <jayarellano831@icloud.com>
2019-01-23 15:21:52 -0800

    Initial Commit

---
+++ tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by Jay Arellano on 1/23/19.
+//  Copyright © 2019 Jay Arellano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
