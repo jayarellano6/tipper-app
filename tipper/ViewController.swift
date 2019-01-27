b9a6014fac597f2c076b75327e478afaebe0c574
Jay Arellano <jayarellano831@icloud.com>
2019-01-27 13:25:39 -0800

    done

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var finalTotalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var settingsControl: UISegmentedControl!
+    @IBOutlet weak var changeSettingsField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        
+        print("tapping on main view")
+        view.endEditing(true);
+        //tipControl.setTitle("18%", forSegmentAt: 1)
+        
+    }
+    @IBAction func settingsOnTap(_ sender: Any){
+        print("tapping on settings view")
+        view.endEditing(true);
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // get the bill amount
+        let bill = Double(billField.text!) ?? 0
+        // calculate the tip an total
+        let tipPercentages = [0.1, 0.15, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        // update tip and total label
+        tipLabel.text = String(format: "$%.2f", tip)
+        finalTotalLabel.text = String(format: "$%.2f", total)
+        
+    }
 }
 
